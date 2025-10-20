9200b5e5a0a347c237b5b0d1fcd4eb1be993a747
Olabode <adesamson0404@gmail.com>
2025-10-20 21:55:16 +0100

    Implement tests for equipment creation and listing
    Added tests for creating and listing equipment.

--- equipment_rental_project/equipment/tests.py
+++ equipment_rental_project/equipment/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Equipment
 
-# Create your tests here.
+
+class EquipmentTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='admin', password='admin123')
+        self.client.force_authenticate(user=self.user)
+        self.equipment_url = reverse('equipment-list')
+        self.equipment_data = {
+            'name': 'Camera',
+            'description': 'DSLR Camera',
+            'is_available': True,
+            'rental_price': 25.0
+        }
+
+    def test_create_equipment(self):
+        response = self.client.post(self.equipment_url, self.equipment_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Equipment.objects.count(), 1)
+
+    def test_list_equipment(self):
+        Equipment.objects.create(**self.equipment_data)
+        response = self.client.get(self.equipment_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
